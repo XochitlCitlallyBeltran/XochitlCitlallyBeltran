40fcce7e19c2241e61fd075abcb0e7c75192a988
Joseph <Joseph>
2021-02-17 12:47:45 -0600

    Modificacion

---
+++ src/Model/Usuario.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Usuario() {
+    }
+
+    /**
+     * 
+     */
+    private String user;
+
+    /**
+     * 
+     */
+    private String psw;
+
+    /**
+     * 
+     */
+    private String correoElectronico1;
+    
+    private String lol;
+    
+    public String modificacion;
+
+}
