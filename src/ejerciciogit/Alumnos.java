d5a1b5afe1457031fb406a98407c08662e995a8d
Joseph <Joseph>
2021-02-17 13:11:31 -0600

    Trabajo

---
+++ src/ejerciciogit/Alumnos.java
@@ -0,0 +1,50 @@
+package ejerciciogit;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Alumnos extends Usuario {
+
+    /**
+     * Default constructor
+     */
+    public Alumnos() {
+    }
+public String matricula;
+public float semestre;
+public String carrera;
+
+public Alumnos(String m,float s,String c){
+    this.matricula=m;
+    this.semestre=s;
+    this.carrera=c;
+}
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public float getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(float semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+}
