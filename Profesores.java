5759b0a2abee94df5d9e0427f9d4073bb9ff396e
EdgarMedinaa <79216079+EdgarMedinaa@users.noreply.github.com>
2021-02-18 12:52:34 -0600

    Update Profesores.java

--- Profesores.java
+++ Profesores.java
@@ -11,5 +11,52 @@ public class Profesores extends Usuario {
      */
     public Profesores() {
     }
+    public String nombre;
+    public String correo;
+    public String materia;
+    public Float añosLaborando;
+    public String sexo;
+    
+    
+   public Profesires(String n, String c, String m, float a, String s){
+    this.nombre=n;
+    this.correo=c;
+    this.materia=m;
+    this.añosLaborando=a;
+    this.sexo=s;
 
+}
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+        
+        public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo){
+        this.correo=correo;
+    }
+     public String getMateria() {
+        return materia;
+    }
+    public void setMateria(String materia){
+        this.materia=materia;
+    }
+   
+     public float getAñoslaborando() {
+        return añosLaborando;
+    }
+    public void setAñoslaborando(float añosLaborando){
+        this.añosLaborando=añosLaborando;
+    }
+    
+     public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo){
+        this.sexo=sexo;
+    }
 }
