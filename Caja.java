fff80821c678ca3220381854ecd7da887bded672
AYGS20 <78193414+AYGS20@users.noreply.github.com>
2021-02-18 18:16:15 -0600

    Update Caja.java
    Se agregaron los atributos y métodos.

--- Caja.java
+++ Caja.java
@@ -10,6 +10,63 @@ public class Caja extends Usuario {
      * Default constructor
      */
     public Caja() {
+    double precio;
+Int descuento;
+String documentos;
+String formapago;
+
+Public void capturarDocumento() {
+
+Scanner capturar = new Sacnner (syatem. In) ;
+Syatem. out. Printin("documento a capturar") ;
+Documento = capturar. next() ;
+
+System.out.printin("forma de pago") ;
+formapago= capturar. next() ;
+
+}
+
+Public void calcularSubtotal() {
+Switch (documento) {
+Case "inscripción":
+Precio = 1500
+Break;
+Case "examen" :
+Precio = 1400
+Break;
+Case "matricula" :
+Precio = 300
+Break;
+Case "recursamiento" :
+Precio = 700
+Break;
+} 
+
+} 
+
+Public void calculardescuento() {
+Switch (documento) {
+Case "inscripción":
+Descuento = 1500
+Break;
+Case "examen" :
+Descuento = 1400
+Break;
+Case "matricula" :
+Descuento = 300
+Break;
+Case "recursamiento" :
+Descuento = 700
+Break;
+}
+public void preciofinal() {
+} 
+
+Public void imprimirTicket() {
+
+}
+
+Public static void main(String [] arga) {    
     }
 
 }
